f8277150aebca0ec20b4a6b7fafb52d58a21ad72
Rustam Rustamov <rustam.r.r.r.r@gmail.com>
2022-10-28 00:47:22 +0300

    Socials-component-added

---
+++ client/src/components/Socials/Socials.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import TelegramIcon from '@mui/icons-material/Telegram';
+
+const Social =() =>{
+    const icons =[<FacebookIcon/>, <TwitterIcon/>, <LinkedInIcon/>, <YouTubeIcon/>, <PinterestIcon/>,<TelegramIcon/>]
+    return (
+        <div className='contacts__socials-container'>
+            {
+                icons.map(icon=>(
+                    <div className='contacts__socials-item'> {icon} </div>))
+            }
+        </div>
+    )
+}
+export default Social

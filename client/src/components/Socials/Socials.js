aebd0062df900e85bdedb4b0cf8f690dcd54b7a8
Rustam Rustamov <rustam.r.r.r.r@gmail.com>
2022-11-05 00:42:20 +0200

    hot-fix-formatting

--- client/src/components/Socials/Socials.js
+++ client/src/components/Socials/Socials.js
@@ -6,16 +6,16 @@ import YouTubeIcon from '@mui/icons-material/YouTube';
 import PinterestIcon from '@mui/icons-material/Pinterest';
 import TelegramIcon from '@mui/icons-material/Telegram';
 
-const Social =() =>{
-    const icons =[<FacebookIcon/>, <TwitterIcon/>, <LinkedInIcon/>, <YouTubeIcon/>, <PinterestIcon/>,<TelegramIcon/>]
-    return (
-        <div className='contacts__socials-container'>
-            {
-                icons.map((icon,i)=>(
-                    // eslint-disable-next-line react/no-array-index-key
-                    <div key={i} className='contacts__socials-item'> {icon} </div>))
-            }
-        </div>
-    )
+const Social = () => {
+  const icons = [<FacebookIcon/>, <TwitterIcon/>, <LinkedInIcon/>, <YouTubeIcon/>, <PinterestIcon/>, <TelegramIcon/>]
+  return (
+    <div className='contacts__socials-container'>
+      {
+        icons.map((icon, i) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <div key={i} className='contacts__socials-item'> {icon} </div>))
+      }
+    </div>
+  )
 }
 export default Social

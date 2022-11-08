e192fe3188a992d320876a0786a83918dfc24752
Rustam Rustamov <rustam.r.r.r.r@gmail.com>
2022-11-08 21:42:32 +0200

    snapshot-test-written

---
+++ client/src/pages/ContactsPage/ContactsPage.test.js
@@ -0,0 +1,9 @@
+import ContactsPage from './ContactsPage'
+import {render} from "@testing-library/react";
+
+describe('ContactsPage',()=>{
+  test('Contacts page renders',()=>{
+    const {asFragment}= render(<ContactsPage/>)
+    expect(asFragment()).toMatchSnapshot()
+  })
+})

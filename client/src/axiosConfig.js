73a890a895bbbff939dc0f6a409c43bab36a6662
Daniel Bliznichenko <danilbliz0201@gmail.com>
2022-12-18 21:25:51 +0200

    feat: change local server url to production

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosConfig = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://nice-teal-skunk-tie.cyclic.app/',
 });
 
 axiosConfig.interceptors.request.use(

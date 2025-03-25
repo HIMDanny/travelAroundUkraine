d63cded3c36d4b8f38bf1e36cac5d989939b4a11
Danny <danilbliz0201@gmail.com>
2025-03-25 23:10:55 +0200

    feat: enhance customer routes
    - Move routes for retrieving the current customer profile and updating the customer password before getting customer by id.
    - Integrated CustomerService and CustomerRepository into the customer routes for improved modularity and maintainability.

--- routes/customers.js
+++ routes/customers.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const customerController = require('../controllers/customers');
+
+const CustomerService = require('../services/customer');
+const CustomerRepository = require('../repositories/customer');
+const CustomerController = require('../controllers/customers');
+
+const customerRepository = new CustomerRepository();
+const customerService = new CustomerService(customerRepository);
+const customerController = new CustomerController(customerService);
 
 // @route   POST /customers
 // @desc    Register customer
@@ -20,6 +27,24 @@ router.get(
   '/',
   passport.authenticate('jwt', { session: false }),
   customerController.getAll.bind(customerController),
+);
+
+// @route   GET /customers/customer
+// @desc    Get current customer profile
+// @access  Private
+router.get(
+  '/customer',
+  passport.authenticate('jwt', { session: false }),
+  customerController.getCurrentCustomer.bind(customerController),
+);
+
+// @route   PUT /customers/password
+// @desc    Update customer password
+// @access  Private
+router.put(
+  '/password',
+  passport.authenticate('jwt', { session: false }),
+  customerController.updatePassword.bind(customerController),
 );
 
 // @route   GET /customers/:id
@@ -47,24 +72,6 @@ router.delete(
   '/:id',
   passport.authenticate('jwt-admin', { session: false }),
   customerController.delete.bind(customerController),
-);
-
-// @route   GET /customers/customer
-// @desc    Get current customer profile
-// @access  Private
-router.get(
-  '/customer',
-  passport.authenticate('jwt', { session: false }),
-  customerController.getCurrentCustomer.bind(customerController),
-);
-
-// @route   PUT /customers/password
-// @desc    Update customer password
-// @access  Private
-router.put(
-  '/password',
-  passport.authenticate('jwt', { session: false }),
-  customerController.updatePassword.bind(customerController),
 );
 
 module.exports = router;
